2e8e501d73b35f7227130fb89655b2d58f549769
minghuidai <matthew.dai@tokiomarinetech.com>
2015-04-08 09:34:32 -0400

    updated to make a utilities with multplse purpose

---
+++ Ming.Tools/Pages/DownloadBingMapShaoe.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ming.Tools.Pages
+{
+    /// <summary>
+    /// Interaction logic for DownloadBingMapShaoe.xaml
+    /// </summary>
+    public partial class DownloadBingMapShaoe : Page
+    {
+        public DownloadBingMapShaoe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            //Microsoft.Maps.SpatialToolbox.Bing.GeoDataServices
+        }
+    }
+}
